e88fb756bbdd37622efe43600664dcec299b2de8
Maria Seichas <85970732+MariaEduardaSeichas@users.noreply.github.com>
2022-02-23 21:25:27 +0000

    Criando método da seleção

--- models/autor.js
+++ models/autor.js
@@ -1,1 +1,11 @@
 const db = require("../db")
+
+class Autor{
+  static async selecionar(){
+    const connect =  await db.connect();
+    return await connect.query("select * from autores");
+  }
+}
+
+module.exports = Autor;
+
