2cc3dc184fefda481e04654a1b8a5c68142679c1
Maria Seichas <85970732+MariaEduardaSeichas@users.noreply.github.com>
2022-03-09 20:53:37 +0000

    Adicionando métodos para inserir itens Modificou a rota

--- models/autor.js
+++ models/autor.js
@@ -5,7 +5,13 @@ class Autor{
     const connect =  await db.connect();
     return await connect.query("select * from autores");
   }
-}
+    static async inserir(data){
+      const connect = await db.connect();
+      const sql = "insert into autores(nome,sobrenome,datanascimento) values($1,$2,$3)"; 
+      const values = [datanome,data.sobrenome,data.datanascimento]
+      return await connect.query(sql,values);
+    }
+  }
 
 module.exports = Autor;
 
