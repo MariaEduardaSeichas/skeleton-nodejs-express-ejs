f2e2c70500b541c9535b47e2723bcff3d65992f5
Maria Seichas <85970732+MariaEduardaSeichas@users.noreply.github.com>
2022-03-16 20:11:16 +0000

    Adicionando métodos para atualizar e deletar

--- models/autor.js
+++ models/autor.js
@@ -1,17 +1,23 @@
 const db = require("../db")
 
-class Autor{
-  static async selecionar(){
-    const connect =  await db.connect();
+class Autor {
+  static async selecionar() {
+    const connect = await db.connect();
     return await connect.query("select * from autores");
   }
-    static async inserir(data){
-      const connect = await db.connect();
-      const sql = "insert into autores(nome,sobrenome,datanascimento) values($1,$2,$3)"; 
-      const values = [datanome,data.sobrenome,data.datanascimento]
-      return await connect.query(sql,values);
-    }
+  
+  static async inserir(data) {
+    const connect = await db.connect();
+    const sql = "insert into autores(nome,sobrenome,datanascimento) values($1,$2,$3)";
+    const values = [datanome, data.sobrenome, data.datanascimento]
+    return await connect.query(sql, values);
   }
+
+  static async atualizar(data, id){
+    const connect = await db.connect();
+    
+  }
+}
 
 module.exports = Autor;
 
--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,19 @@ router.get('/', async function(req, res, next) {
   res.json(autores.rows);
 });
 
+router.get('/inserir', async function (req,res,next){
+  const autores = await Autor.inserir();
+  res.json(autores.rows);
+});
+
+router.get('/atualizar', async function (req,res,next){
+  const autores = await Autor.atualizar();
+  res.json(autores.rows);
+});
+
+router.get('/deletar', async function (req,res,next){
+  const autores = await Autor.deletar();
+  res.json(autores.rows);
+});
+
 module.exports = router;
